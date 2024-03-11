7b80c0c798c5350a5185bf069da6ade93ec09c13
kaiolopes667 <kaiolopes667@gmail.com>
2024-03-11 14:39:13 -0300

    alteração node.js, adicionando config db login

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,22 +1,31 @@
 const express = require('express');
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: 'localhost', // Altere para o host do seu banco de dados
+    user: 'root', // Altere para o usuário do seu banco de dados
+    password: '', // Altere para a senha do seu banco de dados
+    database: 'crud'
+});
 
 const routes = express.Router();
-const users = [{
-    id: 1,
-    nome: 'Kaio',
-    email: 'kaio@gmail.com',
-    password: '123456'
-}]
 
 routes.post('/login', (req, res) => {
     const { email, password } = req.body;
 
-    const user = users.find(user => user.email === email && user.password === password);
-    if (user)
-    {
-        return res.status(200).json(user);
-    }
-    return res.status(401).json({ message: 'Credenciais Inválidas' });
+    connection.query('SELECT * FROM usuarios WHERE email = ? AND password = ?', [email, password], (error, results, fields) => {
+        if (error) {
+            console.error('Erro ao executar a consulta:', error);
+            return res.status(500).json({ message: 'Erro interno do servidor' });
+        }
+
+        if (results.length > 0) {
+            const user = results[0];
+            return res.status(200).json(user);
+        } else {
+            return res.status(401).json({ message: 'Credenciais Inválidas' });
+        }
+    });
 });
 
 module.exports = routes;
